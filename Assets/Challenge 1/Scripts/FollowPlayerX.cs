22252fa55aa75cdeaa62486a5acb6f745f3c2990
Joeback34 <joebackthehero@gmail.com>
2023-02-09 13:17:54 -0600

    lul

---
+++ Assets/Challenge 1/Scripts/FollowPlayerX.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayerX : MonoBehaviour
+{   // Sets the camera position
+    public GameObject plane;
+    private Vector3 offset = new Vector3(40, 0, 10);
+
+    // Update for camera position is called once per frame
+    void Update()
+    {
+        transform.position = plane.transform.position + offset;
+    }
+}
